e055ea7cb6d9aa4544bbd05d1211a1d732e7763d
DTubridy <tubbs@live.ie>
2020-05-18 12:51:46 +0100

    Initial commit

---
+++ projectboard/src/main/java/com/dt/projectboard/service/ProjectTaskService.java
@@ -0,0 +1,22 @@
+package com.dt.projectboard.service;
+
+import com.dt.projectboard.domain.ProjectTask;
+import com.dt.projectboard.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask){
+
+        if(projectTask.getStatus()==null || projectTask.getStatus()==""){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
---
+++ projectboard/src/test/java/com/dt/projectboard/ProjectboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
