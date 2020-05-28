59f959e2bf79368462d2e6f294e1c57340db7669
Dtubridy <tubbs@live.ie>
2020-05-28 20:27:52 +0100

    sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
-import ProjectBoard from "./components/ProjectBoard";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import AddProjectTask from "./components/ProjectTask/AddProjectTask";
-import { Provider } from "react-redux";
-import store from "./store";
-import UpdateProjectTask from "./components/ProjectTask/UpdateProjectTask";
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProjectBoard from './components/ProjectBoard';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AddProjectTask from './components/ProjectTask/AddProjectTask';
+import { Provider } from 'react-redux';
+import store from './store';
+import UpdateProjectTask from './components/ProjectTask/UpdateProjectTask';
 
 class App extends Component {
   render() {
